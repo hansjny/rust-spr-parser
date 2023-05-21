b34a2f9c5e2022aaceb3572eb19a83c6d5fa7b79
Hans Jørgen Nygårdshaug <hans.nyg@gmail.com>
2023-05-21 21:10:27 +0200

    Succesful file opening

--- spr-parser/src/main.rs
+++ spr-parser/src/main.rs
@@ -1,22 +1,36 @@
-use std::env;
-fn main() {
-    let mut filename = String::new();
-    if parse_command_line(&mut filename) {
-        println!("Parsing file: {}", filename);
-    } else {
-        return;
-    }
+use std::{env, io};
+use std::fs::File;
+use std::process;
+
+fn main() -> io::Result<()> {
+
+    let filename: String = match parse_command_line() {
+        Ok(filename) => filename,
+        Err(e) => {
+            println!("Error parsing command line: {}", e);
+            process::exit(1);
+        },
+    };
+
+    let file: File = match File::open(filename) {
+        Ok(file) => file,
+        Err(e) => {
+            println!("Error opening file: {}", e);
+            process::exit(1);
+        },
+    };
+    print!("File opened successfully");
+    Ok(())
 }
 
 
-fn parse_command_line(filename: &mut String) -> bool {
+fn parse_command_line() -> Result<String, &'static str> {
     let args: Vec<String> = env::args().collect();
 
     if args.len() < 2 { 
-        println!("Please provide spr file path via  /path/to/file.spr");
-        return false;
+        Err("Please provide spr file path via  /path/to/file.spr")
+    } else
+    {
+        Ok(args[1].clone())
     }
-
-    *filename = args[1].clone();
-    true
 }

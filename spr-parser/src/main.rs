21e5995bac1f448b5921d42d35e122e7d08ea92a
Hans Jørgen Nygårdshaug <hans.nyg@gmail.com>
2023-05-21 20:36:45 +0200

    Add: File input path parsing, datafiles, gitignore

--- spr-parser/src/main.rs
+++ spr-parser/src/main.rs
@@ -1,3 +1,22 @@
+use std::env;
 fn main() {
-    println!("Hello, world!");
+    let mut filename = String::new();
+    if parse_command_line(&mut filename) {
+        println!("Parsing file: {}", filename);
+    } else {
+        return;
+    }
+}
+
+
+fn parse_command_line(filename: &mut String) -> bool {
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() < 2 { 
+        println!("Please provide spr file path via  /path/to/file.spr");
+        return false;
+    }
+
+    *filename = args[1].clone();
+    true
 }
